5b65e882c8b1289e53792f438ef87b297192aa16
Aradhya Nigam <87924213+aradhyanigam17@users.noreply.github.com>
2022-02-10 21:45:17 +0530

    Time: 1 ms (37.82%), Space: 42.8 MB (5.85%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -1,35 +1,42 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
+
+//   public class TreeNode {
+//       int val;
+//       TreeNode left;
+//       TreeNode right;
+//       TreeNode() {}
+//       TreeNode(int val) { this.val = val; }
+//       TreeNode(int val, TreeNode left, TreeNode right) {
+//             this.val = val;
+//             this.left = left;
+//             this.right = right;
+//         }
+//     }
+
+   class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        ArrayList<Integer> list = new ArrayList<>() ;
+       
+        List<Integer> mainList = new ArrayList<>() ;
         
-        postorder( root , list) ;
+        if(root == null) return mainList ;
         
-        return list ;
-               
+        Stack<TreeNode> stack1 = new Stack<>() ;
+        Stack<TreeNode> stack2 = new Stack<>() ;
+        
+        stack1.push(root) ;
+        
+        while(!stack1.isEmpty()){
+            root = stack1.pop() ;
+            stack2.push(root);
+            
+            if(root.left != null) stack1.push(root.left);
+            if(root.right != null) stack1.push(root.right) ;
+            
+        }
+        
+        while(!stack2.isEmpty()){
+            mainList.add(stack2.pop().val);
+        }
+        return mainList ;
     }
-    
-    public void postorder(TreeNode node , List<Integer> list){
-        
-        if(node == null) return ;
-        
-        postorder(node.left, list) ;
-        postorder(node.right , list) ;
-        list.add(node.val) ;
-        
-    }
+       
 }
