392c8aac9258b889c17ba7a776f7f8d8c67afbf8
Aradhya Nigam <87924213+aradhyanigam17@users.noreply.github.com>
2022-02-11 12:50:22 +0530

    Time: 1 ms (38.24%), Space: 42.2 MB (10.69%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -1,42 +1,52 @@
-
-//   public class TreeNode {
-//       int val;
-//       TreeNode left;
-//       TreeNode right;
-//       TreeNode() {}
-//       TreeNode(int val) { this.val = val; }
-//       TreeNode(int val, TreeNode left, TreeNode right) {
-//             this.val = val;
-//             this.left = left;
-//             this.right = right;
-//         }
-//     }
-
-   class Solution {
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-       
-        List<Integer> mainList = new ArrayList<>() ;
+        List<Integer> mainList = new ArrayList<Integer>() ;
         
         if(root == null) return mainList ;
         
-        Stack<TreeNode> stack1 = new Stack<>() ;
-        Stack<TreeNode> stack2 = new Stack<>() ;
+        Stack <TreeNode> stack1 = new Stack<>() ;
         
-        stack1.push(root) ;
+        TreeNode current = root ; // going to travel through all left nodes 
         
-        while(!stack1.isEmpty()){
-            root = stack1.pop() ;
-            stack2.push(root);
+        while(current != null || !stack1.isEmpty()){
             
-            if(root.left != null) stack1.push(root.left);
-            if(root.right != null) stack1.push(root.right) ;
+            if(current != null){
+                stack1.push(current) ;
+                current = current.left ;
+                
+            }
             
-        }
-        
-        while(!stack2.isEmpty()){
-            mainList.add(stack2.pop().val);
+            else{
+                TreeNode temp = stack1.peek().right ;
+                
+                if(temp == null){
+                    temp = stack1.pop();
+                    mainList.add(temp.val) ;
+                    while(!stack1.isEmpty() && temp == stack1.peek().right){
+                        temp = stack1.pop();
+                        mainList.add(temp.val) ;
+                        
+                    }
+                }
+                else current = temp ;
+            }
+            
         }
         return mainList ;
     }
-       
 }
