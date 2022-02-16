40b587cefa83bc72ada93338dd4274dfc1f6c9a2
Aradhya Nigam <87924213+aradhyanigam17@users.noreply.github.com>
2022-02-16 22:18:00 +0530

    Time: 5 ms (13.54%), Space: 43 MB (10.08%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -15,25 +15,47 @@
  */
 
 
-class Solution {
-     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<Integer>();
-        rightView(root, result, 0);
-        return result;
-    }
-    
-    public void rightView(TreeNode curr, List<Integer> result, int currDepth){
-        if(curr == null){
-            return;
+
+    class Pair2{
+        int x ;
+        int y ;
+        TreeNode node ;
+
+        public Pair2(int xaxis , TreeNode nod){
+            this.x = xaxis ;
+          
+            this.node = nod ;
         }
-        
-        if(currDepth == result.size()){
-            result.add(curr.val);
-        }
-        
-        rightView(curr.right, result, currDepth + 1);
-        rightView(curr.left, result, currDepth + 1);
-        
+
     }
-    
-}
+    class Solution {
+        public List<Integer> rightSideView(TreeNode root) {
+            List<Integer> list = new LinkedList<Integer>() ;
+            if(root == null) return list ;
+
+            TreeMap<Integer,Integer> map = new TreeMap<>() ;
+
+            Queue<Pair2> q = new LinkedList<>() ;
+
+            q.add(new Pair2(0  , root));
+
+            while(!q.isEmpty()){
+
+                Pair2 it = q.remove() ;
+                int x = it.x ;
+                TreeNode node = it.node ;
+                
+                map.put(x , node.val);
+
+                if(node.left != null){
+                    q.add(new Pair2(x + 1 ,node.left));
+                }
+                if(node.right != null){
+                    q.add(new Pair2(x + 1 , node.right));
+                }
+            }
+
+            list.addAll(map.values());
+            return list;
+        }
+    }
