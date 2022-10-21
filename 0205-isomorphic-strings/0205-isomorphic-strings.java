28eb99e41dca4a07060ddde7039564c843c95413
Aradhya Nigam <87924213+aradhyanigam17@users.noreply.github.com>
2022-10-21 17:02:53 +0530

    Time: 7 ms (89.07%), Space: 42 MB (95.82%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,28 +1,19 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length() == 1){
-            return true ;
+        if(s.length() != t.length())
+            return false;
+        // Create two maps for s & t strings...
+        int[] map1 = new int[256];
+        int[] map2 = new int[256];
+        // Traverse all elements through the loop...
+        for(int idx = 0; idx < s.length(); idx++){
+            // Compare the maps, if not equal, return false...
+            if(map1[s.charAt(idx)] != map2[t.charAt(idx)])
+                return false;
+            // Insert each character if string s and t into seperate map...
+            map1[s.charAt(idx)] = idx + 1;
+            map2[t.charAt(idx)] = idx + 1;
         }
-        
-        Map<Character,Character> map = new HashMap<>() ;
-        
-        for(int i = 0 ; i < s.length() ;i++){
-            
-            if(map.containsKey(s.charAt(i))){
-                char ch = s.charAt(i) ;
-                if(map.get(ch) != t.charAt(i)) {
-                    return false;
-                }
-            }
-            
-            else if(map.containsValue(t.charAt(i))){
-                return false ;
-            } 
-
-            else {
-                map.put(s.charAt(i),t.charAt(i)) ;
-            } 
-        }
-        return true ;
+        return true;
     }
 }
