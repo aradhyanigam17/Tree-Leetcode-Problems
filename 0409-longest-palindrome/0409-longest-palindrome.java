113b1245890500ad6fdb301590d17ebcc7c7af75
Aradhya Nigam <87924213+aradhyanigam17@users.noreply.github.com>
2022-10-28 21:51:46 +0530

    Time: 12 ms (36.49%), Space: 42.3 MB (58.22%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        if(s.length() == 1){
+            return 1 ;
+        }
+        
+        Map<Character, Integer> map = new HashMap<>() ;
+        
+        for(char ch : s.toCharArray()){
+            if(map.containsKey(ch))
+            {
+                map.put(ch,map.get(ch) + 1);
+                
+            }   
+            
+            else map.put(ch,1) ;
+        }
+        
+        int len = 0 ;
+        boolean odd = false ;
+        for(int count : map.values())
+        {
+            if(count % 2 == 0 ) len += count ;
+            else {
+                odd = true ;
+                len += count - 1 ;
+            }
+        }        
+        
+        if(odd){
+            len += 1 ;
+        }
+        return len ;
+    }
+}
