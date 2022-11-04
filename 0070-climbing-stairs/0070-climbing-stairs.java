f78fc9eccab4ac85f6f7d08f35ce6b85c33bd3ad
Aradhya Nigam <87924213+aradhyanigam17@users.noreply.github.com>
2022-11-04 10:42:03 +0530

    Time: 0 ms (100.00%), Space: 41 MB (29.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,63 +1,18 @@
-class Solution{
-    public int climbStairs(int n){
-        if(n < 3){
-            return n ;
-        }
-        
-        int[] storage = new int[n + 1] ; 
-        
-        storage[0] = 0 ;
-        storage[1] = 1 ;
-        storage[2] = 2 ;
-        
-        for(int i = 3 ;i <= n ;i++){
-            storage[i] = storage[i - 1] + storage[i - 2] ;
-        }
-        
-        
-        return storage[n] ;
-    }
-}
-
-/*
 class Solution {
     public int climbStairs(int n) {
-          
+        if(n <= 3){
+            return n ; 
+        }
         int[] storage = new int[n + 1] ;
         
-        return helper(n , storage) ;
-        
-    }
-    
-    public int helper(int n , int[] storage){
-        if(n < 3){
-            return n ;
+        for(int i = 0 ;i <= n ;i++){
+            if(i <= 3){
+                storage[i] = i ;
+            }
+            
+            else storage[i] = storage[i - 1] + storage[i - 2] ;
         }
         
-        if(storage[n] != 0){
-            return storage[n] ;
-        }
-        
-        storage[n] = helper(n - 1 , storage) + helper(n - 2 , storage) ;
-        
-        return storage[n] ;
+        return storage[n] ;     
     }
 }
-*/
-
-
-
-/*
-
-class Solution {
-    public int climbStairs(int n) {
-          
-        if(n < 3){
-            return n ;
-        }
-        return climbStairs(n - 1) + climbStairs(n - 2) ;
-        
-    }
-}
-
-*/
